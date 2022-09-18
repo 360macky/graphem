ffd25259493c9fc8c679bf5174a696339b0636f2
Marcelo Arias <hello@marceloarias.com>
2022-09-18 14:42:55 -0500

    Update index.ts test code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-// DO NOT INSTALL. This are alpha tests for flight.
-
+// DO NOT INSTALL.
 import { hello } from "hello-world-js";
 
-const testVariable = 10;
-
-(async () => {
-  console.log(hello());
-  const testApi = "https://api.spacexdata.com/v5/launches/latest";
-  const response = await fetch(testApi);
-  const data = await response.json();
-  console.log(data);
-})();
-
-export default testVariable;
+export default function Graphem(configuration: any) {
+  return function install() {
+    console.log("Graphem installed");
+    console.log(hello());
+  };
+}
